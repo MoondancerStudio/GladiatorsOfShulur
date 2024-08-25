6da2bf4c397d486f5cfd574c7855bc5f4fd031e3
Mark Kalinovits <kalee.mark@gmail.com>
2024-08-25 08:16:56 +0200

    chore: copy proprt scene from other project

---
+++ Assets/Scenes/BoardTest/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Scenes.BoardTest.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private int sizeX = 5, sizeY = 5;
+        [SerializeField] private BoardTile tilePrefab;
+
+        [SerializeField] private Transform cam;
+
+        public void Start()
+        {
+            BuildBoardGrid();
+
+            PositionCamera();
+        }
+
+        private void BuildBoardGrid()
+        {
+
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    BoardTile spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                    spawnedTile.name = $"Tile {x} {y}";
+                    bool isOffset = IsOffset(x, y);
+                    spawnedTile.Init(isOffset);
+                }
+            }
+        }
+
+        /**
+         * (0, 1); (1, 2)
+         */
+        private bool IsOffset(int x, int y)
+        {
+            return (x + y) % 2 != 0;
+        }
+        
+        private void PositionCamera()
+        {
+            cam.position = new Vector3(sizeX / 2f - 0.5f, sizeY / 2f -0.5f, -10);
+        }
+    }
+}
